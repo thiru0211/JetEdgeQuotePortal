b9fe1f119134e70af87884706af198c6a1c0a0d4
thiru0211 <0211thiru@gmail.com>
2024-07-16 16:15:17 +0530

    Modify the runner class names and add test in the end of the runner class

---
+++ src/test/java/runnerClass/JE_QP_3_PricingRunnerTest.java
@@ -0,0 +1,19 @@
+package runnerClass;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features ="C:\\Users\\thirumaran\\eclipse-workspace\\JetEdgeQuotePortal\\src\\test\\resources\\featureFiles\\JE_QP_3_Pricing.feature",
+glue = "stepDefinition",
+tags="@TC_46",
+plugin = {"pretty","html:Reports/TestReport.html",
+		"json:target/cucumber/cucumber.json",
+		"html:Reports/cucumber-reports.html"
+		})
+public class JE_QP_3_PricingRunnerTest {
+
+	
+}
---
+++ src/test/java/runnerClass/JE_QP_7_ReportsRunnerTest.java
@@ -0,0 +1,21 @@
+package runnerClass;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features ="C:\\Users\\thirumaran\\eclipse-workspace\\JetEdgeQuotePortal\\src\\test\\resources\\featureFiles\\JE_QP_7_Reports.feature",
+glue = "stepDefinition",
+//dryRun = true,
+tags="@TC_08",
+plugin = {"pretty","html:Reports/TestReport.html",
+		"json:target/cucumber/cucumber.json",
+		"html:Reports/cucumber-reports.html"
+		})
+
+public class JE_QP_7_ReportsRunnerTest {
+
+	
+}
